9cd083a45d37b9f3517938a20828e52de0ba9628
Mamogapi <117472107+Mamogapi@users.noreply.github.com>
2022-11-05 15:52:15 +0200

    Update footer.tsx

--- finalportfolio-site/components/footer.tsx
+++ finalportfolio-site/components/footer.tsx
@@ -1,5 +1,5 @@
-import {FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faGithub} from "@fortawesome/free-brands-svg-icons"
+import {FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () =>{
     const icon =<FontAwesomeIcon icon ={faGithub}/> ;
